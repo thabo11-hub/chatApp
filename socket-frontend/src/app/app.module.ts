0c03be32869d046e09591d806cb2c60830684d63
thabo sodi <thabo.sodi@dadevs.co.za>
2022-07-07 22:54:23 +0200

    socket setup

--- socket-frontend/src/app/app.module.ts
+++ socket-frontend/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatInboxComponent
   ],
   imports: [
     BrowserModule,
---
+++ socket-frontend/src/app/chat-inbox/chat-inbox.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client';
+
+const SOCKET_ENDPOINT = 'localhost:3000';
+
+@Component({
+  selector: 'app-chat-inbox',
+  templateUrl: './chat-inbox.component.html',
+  styleUrls: ['./chat-inbox.component.scss']
+})
+export class ChatInboxComponent implements OnInit {
+
+  socket: any;
+  constructor() { }
+  ngOnInit() {
+    this.setupSocketConnection();
+  }
+  setupSocketConnection() {
+     this.socket = io;
+     
+  }
+}

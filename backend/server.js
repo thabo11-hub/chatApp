c2eca86fbaa9454062dd192491c84f521531a218
thabo sodi <thabo.sodi@dadevs.co.za>
2022-07-07 21:48:23 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+
+app.get('/', (req, res) => res.send('hello server!'));
+  http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
